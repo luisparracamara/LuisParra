037db4b22baa024d1e35781f83cc74d4f04a5028
Luis <luisparracamara@gmail.com>
2019-06-10 23:40:12 -0700

    Updates

---
+++ precache-manifest.3a87bcd9a9014cb2310e994df710eaf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8886684724cfcc41af73e305801ca9",
+    "url": "/luisparra/index.html"
+  },
+  {
+    "revision": "da7b34508c680e7d41fb",
+    "url": "/luisparra/static/css/main.3efc0d89.chunk.css"
+  },
+  {
+    "revision": "ae1871e95641ca3f8681",
+    "url": "/luisparra/static/js/2.aad42321.chunk.js"
+  },
+  {
+    "revision": "da7b34508c680e7d41fb",
+    "url": "/luisparra/static/js/main.f164ac61.chunk.js"
+  },
+  {
+    "revision": "7318ab94af6b3615847b",
+    "url": "/luisparra/static/js/runtime~main.38d4b594.js"
+  }
+]);
